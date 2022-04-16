417c18d74b9fa5040984c4b248b0cdd9d02ac291
phipham124 <49842519+phipham124@users.noreply.github.com>
2022-04-16 23:01:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    titleWeb: "Hi Em",
+    introTitle: 'Phạm Đức Minh',
+    introDesc: `Cảm ơn em vì đã xuất hiện trong cuộc đời anh`,
+    btnIntro: 'Đây là bất ngờ dành cho em (Hãy nhấp vào đây)',
+    title: 'Thay lời chúc ngủ ngon',
+    desc: 'Anh muốn hỏi là em có thích anh không. Cơ mà anh biết câu trả lời rồi <3',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Em iu ngủ ngon nha, mau hết bệnh nữa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
