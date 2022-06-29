78057ab03a70c26e53270f8a00737b7ae90e9d2d
phipham124 <49842519+phipham124@users.noreply.github.com>
2022-06-30 05:23:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Hello em",
-    introTitle: 'Gửi Phạm Đức Minh',
+    introTitle: 'Gửi Nguyễn Thế Anh',
     introDesc: `Cảm ơn em vì đã xuất hiện trong cuộc đời anh`,
     btnIntro: 'Đây là bất ngờ anh dành cho em <3 (Nhấp vào)',
-    title: 'Cảm ơn em hôm ấy đã chúc mừng sinh nhật cho anh và cho anh cơ hội được yêu em',
+    title: 'Cảm ơn em vì đã xuất hiện trong cuộc đời anh',
     desc: 'Em có yêu anh không, Cơ mà anh biết câu trả lời rồi',
-    btnYes: 'Yêu lắm Kiki <3',
+    btnYes: 'Yêu lắm <3',
     btnNo: 'Không có yêu đâu ',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu mình <3',
-    btnReply: 'Gửi cho Phi <3',
-    reply: 'Em iu anh Phi Phạm',
-    mess: 'Anh yêu em nhiều lắm Minh béo Kiki',
-    messDesc: 'Đây là món quà hôm qua anh hứa này. Chúc em ngủ ngon và mau hết bệnh nha. Iu emmmm ',
-    btnAccept: 'Dạ kiki',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    btnReply: 'Gửi cho Phi be de<3',
+    reply: 'Em iu anh ',
+    mess: 'Anh yêu em nhiều lắm',
+    messDesc: 'Chào buổi sáng người yêu của anh. Anh yêu Thế Anh của anh nhiều lắm',
+    btnAccept: '<3<3<3',
+    //messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
